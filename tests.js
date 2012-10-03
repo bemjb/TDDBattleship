b28bc0cfe028f329191f1c8d76715b4b965ee06b
Bem Jones-Bey <bjonesbe@adobe.com>
2012-10-03 13:55:53 -0700

    Add namespace for tests.

--- tests.js
+++ tests.js
@@ -1,37 +1,39 @@
-var game;
-module("Game Tests", {
-    setup: function () {
-        game = new Battleship.Game();
-        game.players = [ new MockPlayer("Player 1"), new MockPlayer("Player 2") ];
-    },
-    teardown: function () {
-        game = null;
-    }
-});
-test("Started Game in Ship Placement", function() {
-    equal(game.state, Battleship.Game.State.ShipPlacement, "Check state");
-});
-test("Whose Turn is it initially?", function() {
-    equal(game.activePlayer(), game.players[0]);
-});
-test("Does turn advance properly?", function () {
-    game.finishTurn();
-    equal(game.activePlayer(), game.players[1]);
-});
-test("Does turn wrap around properly?", function () {
-    var index;
-    for (index = 0; index < game.players.length; index++) {
+(function () {
+    var game;
+    module("Game Tests", {
+        setup: function () {
+            game = new Battleship.Game();
+            game.players = [ new MockPlayer("Player 1"), new MockPlayer("Player 2") ];
+        },
+        teardown: function () {
+            game = null;
+        }
+    });
+    test("Started Game in Ship Placement", function() {
+        equal(game.state, Battleship.Game.State.ShipPlacement, "Check state");
+    });
+    test("Whose Turn is it initially?", function() {
+        equal(game.activePlayer(), game.players[0]);
+    });
+    test("Does turn advance properly?", function () {
         game.finishTurn();
-    }
-    equal(game.activePlayer(), game.players[0]);
-});
-test("Is the game over?", function () {
-    // Mocks
-    game.players[1].hasActiveShips = function () {
-        return false;
-    };
+        equal(game.activePlayer(), game.players[1]);
+    });
+    test("Does turn wrap around properly?", function () {
+        var index;
+        for (index = 0; index < game.players.length; index++) {
+            game.finishTurn();
+        }
+        equal(game.activePlayer(), game.players[0]);
+    });
+    test("Is the game over?", function () {
+        // Mocks
+        game.players[1].hasActiveShips = function () {
+            return false;
+        };
 
-    //Test
-    game.finishTurn();
-    equal(game.state, Battleship.Game.State.GameOver, "Check state");
-});
+        //Test
+        game.finishTurn();
+        equal(game.state, Battleship.Game.State.GameOver, "Check state");
+    });
+})();
